81a8a66819196e0025fb52bd810b27193d328199
Pedro Beraldin <pberaldin11@gmail.com>
2022-03-16 19:58:02 -0300

    inflacao/selic

--- bancocentralbrasil/pedro-teste.py
+++ bancocentralbrasil/pedro-teste.py
@@ -2,10 +2,8 @@ import bc.bancocentral as bc
 from bc.bancocentral import Inflacao
 from bc.bancocentral import Selic
 
-
 inflacao = Inflacao()
-print('%s' % inflacao.get_acumulada_tax())
-
+print(f"infração: {'%s' % inflacao.get_acumulada_tax()}")
 
 selic = bc.Selic()
-print(selic.get_selic_meta())
+print(f"selic: {selic.get_selic_meta()}")
